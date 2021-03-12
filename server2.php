ff7d5d467fab4cbcd367f1072a2d6cafac4f2122
pj8912 <59218902+pj8912@users.noreply.github.com>
2021-03-12 22:45:04 +0530

    Update server2.php
    included server1.php to make it look continuous without reloading or refreshing

--- server2.php
+++ server2.php
@@ -10,13 +10,20 @@ $task = htmlentities($_POST['tasks']);
 
 $sql = mysqli_query($conn, "INSERT INTO todo(tasks) VALUES('$task')");
 
-
 if($sql)
-{
-  header("Location: index.php");
-  exit();
+{include 'server1.php';
+
+
+ 
 
 }
+  
+// if($sql)
+// {
+//   header("Location: index.php");
+//   exit();
+
+// }
 // echo 'works';
 }
  ?>
